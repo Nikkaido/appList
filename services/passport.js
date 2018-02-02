a40f64f39963669fe6bc6fe8d55ff38682f6cb50
Nikkaido <nik_1210@inbox.ru>
2018-02-03 03:20:08 +0600

    initial

--- services/passport.js
+++ services/passport.js
@@ -1,8 +1,19 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
+const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id,done)=>{
+    User.findById(id).then((user)=>{
+        done(null, user);
+    });
+});
 
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
@@ -10,9 +21,18 @@ passport.use(new GoogleStrategy({
     callbackURL: '/auth/google/callback'
 },
 (accessToken, refreshToken, profile, done) => {
-            console.log('access token', accessToken);
-            console.log('refresh token', refreshToken);
-            console.log('profile: ', profile);
+    User.findOne({googleId:profile.id})
+    .then(existingUser =>{
+        if(existingUser){
+            // we already have a record with the given profile
+            done(null, existingUser);
+        }else{
+            //we don't have a user with this ID
+             new User({ googleId: profile.id})
+            .save()
+            .then(user => done(null, user));
+            }
+        });     
         }
     )
 );
