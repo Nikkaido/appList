b6a3953fe2f4e5f845107f0179610e7ab29dbbb7
Nikkaido <nik_1210@inbox.ru>
2018-03-17 22:54:00 +0600

    form btn cancel

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -5,17 +5,12 @@ import { reduxForm, Field } from 'redux-form';
 import {Link} from 'react-router-dom';
 import SurveyField from './SurveyField';
 import validateEmails from '../../utils/validateEmails';
+import formFields from './formFields';
 
-const FIELDS =[
-    {label: 'Survey Title', name:'title'},
-    {label: 'Subject Line', name:'subject'},
-    {label: 'Email', name:'body'},
-    {label: 'Recipients List', name:'emails'}
-];
 
 class SurveyForm extends Component {
     renderFields(){
-        return _.map(FIELDS, ({ label, name }) =>{
+        return _.map(formFields, ({ label, name }) =>{
             return (
                 <Field 
                     key={name}
@@ -51,7 +46,7 @@ function validate(values){
 
     errors.emails = validateEmails(values.emails || '');
 
-    _.each(FIELDS, ({name})=>{
+    _.each(formFields, ({name})=>{
         if(!values[name]){
             errors[name] = 'you must provide a value';
         }
@@ -64,5 +59,6 @@ function validate(values){
 
 export default reduxForm({
     validate,
-    form:'surveyForm'
+    form:'surveyForm',
+    destroyOnUnmount: false
 })(SurveyForm);
